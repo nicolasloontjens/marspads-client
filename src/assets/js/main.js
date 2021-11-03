b525130af06eb2af149b41ac4ebc99fd384ebc66
Matthias Blomme <matthias.blomme@howest.be>
2021-11-03 11:00:57 +0100

    Initial commit

---
+++ src/assets/js/main.js
@@ -0,0 +1,17 @@
+let config;
+let api;
+
+document.addEventListener("DOMContentLoaded", init);
+
+async function init() {
+    config = await loadConfig();
+    api = `${config.host ? config.host + '/': ''}${config.group ? config.group + '/' : ''}api/`;
+
+    // Very small proof of concept.
+    poc();
+}
+
+async function loadConfig() {
+    const response = await fetch("config.json");
+    return response.json();
+}
