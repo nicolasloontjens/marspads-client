3a699e2f3300858cdb87c3a444a0384acae3ffe3
stijn <stijn.follet@student.howest.be>
2021-11-09 14:19:57 +0100

    Added a marker tot the current location.

--- src/assets/js/maps.js
+++ src/assets/js/maps.js
@@ -1,30 +1,23 @@
 "use strict"
 
-
-
 document.addEventListener("DOMContentLoaded", init);
-
-
-const map = new ol.Map({
-    target: 'centra-map',
-    layers: [
-        new ol.layer.Tile({
-            source: new ol.source.OSM()
-        })
-    ],
-    view: new ol.View({
-        center: ol.proj.fromLonLat([4.34878, 50.85045]),
-        zoom: 10
-    })
-})
 
 async function init() {
     console.log("Maps loaded");
-    creatingMaps();
+    getLocation();
 
 }
 
-function creatingMaps(){
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(creatingMaps);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+
+}
+
+function creatingMaps(position) {
     const map = new ol.Map({
         target: 'map',
         layers: [
@@ -33,9 +26,35 @@ function creatingMaps(){
             })
         ],
         view: new ol.View({
-            center: ol.proj.fromLonLat([4.34878, 50.85045]),
+            center: ol.proj.fromLonLat([position.coords.longitude, position.coords.latitude]),
             zoom: 10
         })
+
     })
 
+
+    let markerLayer = new ol.layer.Vector({
+        source: new ol.source.Vector({
+            features: [
+                new ol.Feature({
+                    geometry: new ol.geom.Point(ol.proj.fromLonLat([position.coords.longitude, position.coords.latitude]))
+                })
+            ]
+
+        }
+        ), style: new ol.style.Style({
+            image: new ol.style.Icon({
+                src: "assets/images/marker.png",
+                anchor: [0.5, 1]
+            })
+
+
+        })
+    });
+    map.addLayer(markerLayer);
+
 }
+
+
+
+
