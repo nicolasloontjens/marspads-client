e1df44387a032d01945361465835d9453439dc56
stijn <stijn.follet@student.howest.be>
2021-11-18 14:40:01 +0100

    Added an audiofile and it's now possible to change the volume of the played audio with the sliders.

--- src/assets/js/audioControl.js
+++ src/assets/js/audioControl.js
@@ -4,16 +4,55 @@ document.addEventListener("DOMContentLoaded", init);
 
 async function init() {
 
+    loadAudio();
     document.querySelector("#muteAll").addEventListener("click", muteAll);
-    document.querySelectorAll(".slider").forEach(e => e.addEventListener("touchend", getSelectedSlider));
+    document.querySelectorAll(".slider").forEach(e => e.addEventListener("change", getSelectedSlider));
+    document.querySelector(".play").addEventListener("click", playSoundAgain);
 }
 
-function muteAll(){
-
+function playSoundAgain() {
+    document.querySelector("#trafficAudioFile").play();
 }
 
-function getSelectedSlider(ev){
+function loadAudio() {
+    let promise = document.querySelector("#trafficAudioFile").play();
+
+    if (promise !== undefined) {
+        promise.then(_ => {
+            console.log("Autoplay started.")
+        }).catch(error => {
+            console.log("Autoplay was prevented.", error);
+            document.querySelector(".play").style.display = "block";
+        });
+    }
+}
+
+function muteAll() {
+    document.querySelector("#trafficAudioFile").muted = true;
+}
+
+function getSelectedSlider(ev) {
     let sliderName = ev.currentTarget.getAttribute("data-slider");
     let sliderValue = ev.target.value;
-    console.log(sliderName ,sliderValue);
+    let convertedValue = sliderValue / 10;
+    console.log(sliderName, convertedValue);
+    setVolumeOfAudio(sliderName, convertedValue);
 }
+
+function setVolumeOfAudio(sliderName, sliderValue) {
+    if (sliderName === "media") {
+
+    }
+
+    if (sliderName === "talking") {
+
+    }
+    if (sliderName === "traffic") {
+        document.querySelector("#trafficAudioFile").volume = sliderValue;
+    }
+    if (sliderName === "constructionNoises") {
+
+    }
+
+}
+
