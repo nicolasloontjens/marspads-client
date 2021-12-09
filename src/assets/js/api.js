5d0139cec09ec44f9e0567a3f86de355e76fd278
nicolas.loontjens <nicolas.loontjens@student.howest.be>
2021-12-09 16:44:42 +0100

    able to receive notifications from another contact

--- src/assets/js/api.js
+++ src/assets/js/api.js
@@ -1,17 +1,5 @@
 let statusCode;
 
-function poc() {
-    const messageBody = {
-        "quote": "some quote"
-    };
-
-    get("quotes");
-    get("quotes/1");
-    post("quotes", messageBody);
-    put("quotes/1", messageBody);
-
-}
-
 function get(uri, successHandler = logJson, failureHandler = logError) {
     const request = new Request(api + uri, {
         method: 'GET',
@@ -108,7 +96,9 @@ function addUserContact(idToAdd) {
 }
 
 function removeUserContact(idToRemove) {
-    remove(`user/${getMarsID()}/contacts/remove/${idToRemove}`);
+    fetch(`https://project-ii.ti.howest.be/mars-17/api/user/${getMarsID()}/contacts/remove/${idToRemove}`,{
+        method: 'DELETE'
+    })
 }
 
 async function getAllChats() { //get a list of all chatid's and their corresponding user
--- src/assets/js/contactPage.js
+++ src/assets/js/contactPage.js
@@ -1,48 +1,97 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", init);
+let config = null;
+let api = null;
+let sendToServer = null;
 
 async function init() {
     let contacts = await getUserContacts();
     contacts.sort((a,b) => a.name.localeCompare(b.name));
+    config = await loadConfig();
+    api = `${config.host ? config.host + '/' : ''}${config.group ? config.group + '/' : ''}api/`;
     await insertContactsIntoHTML(contacts)
-    document.querySelectorAll(".contactItem").forEach(item => {
-        item.addEventListener("click", getSelectedContactName);
-    });
-    /*
-    todo:
-    add remove functionality
-    */
     document.querySelector("#search").addEventListener("keyup", searchInputField);
-
+    sendToServer = openSocket()
 }
 
 async function insertContactsIntoHTML(contacts){
-    //contacts = await filterContactsWithExistingChats(contacts)
+    document.querySelector("ul").innerHTML = ""
     let chats = await getAllChats();
+    let chatidscontactids = chats.map(chat => {return {contactid: chat.contactid,chatid:chat.chatid}})
+    console.log(chatidscontactids)
     chats = chats.map((chat) => chat.contactid)
     contacts.forEach(contact => {
         if(chats.includes(contact.contactid)){
+            let chatid = 0
+            chatidscontactids.forEach(object => {
+                if(object["contactid"] === contact.contactid){
+                    chatid = object["contactid"]
+                }
+            })
             document.querySelector("#ulContactList").innerHTML  += `<li><div id="${contact.contactid}">
                 <a href="#" class="contactItem" data-contactName="${contact.name}">${contact.name}</a>
-                <a href="#" id="contactoption" class="contactoption-hidden">Go to Chat</a>
+                <a href="#" type="contactoption" class="contactoption-hidden" data-chatid="${chatid}" data-type="gotochat">Go to Chat</a>
+                <a href="#" type="contactoption" class="contactoption-hidden" data-contactid="${contact.contactid}">Remove contact</a>
                 </div>
             </li>`
-        }
-        document.querySelector("#ulContactList").innerHTML  += `<li><div id="${contact.contactid}" >
+        }else{
+            document.querySelector("#ulContactList").innerHTML  += `<li><div id="${contact.contactid}" >
             <a href="#" class="contactItem" data-contactName="${contact.name}">${contact.name}</a>
-            <a href="#" id="contactoption" class="contactoption-hidden">Send chat request</a> 
+            <a href="#" type="contactoption" class="contactoption-hidden" data-contactid="${contact.contactid}" data-type="sendrequest">Send chat request</a>
+            <a href="#" type="contactoption" class="contactoption-hidden" data-contactid="${contact.contactid}">Remove contact</a> 
             </div>
         </li>`
+        }
     })
+    document.querySelectorAll(".contactItem").forEach(item => {
+        item.addEventListener("click", getSelectedContactName);
+    });
+}
+
+async function updateContacts(){
+    let contacts = await getUserContacts();
+    insertContactsIntoHTML(contacts)
 }
 
 function getSelectedContactName(e) {
-    document.querySelectorAll("div #contactoption").forEach((elem) => {
+    document.querySelectorAll('div a[type="contactoption"]').forEach((elem) => {
         elem.setAttribute("class","contactoption-hidden")
     })
     let parentelem = e.target.parentElement
-    parentelem.getElementsByTagName("a")[1].setAttribute("class","contactoption")
+    let action1 = parentelem.getElementsByTagName("a")[1]
+    action1.setAttribute("class","contactoption")
+    action1.addEventListener("click",goToChatOrSendChatRequest)
+    let removecontact = parentelem.getElementsByTagName("a")[2]
+    removecontact.setAttribute("class","contactoption")
+    removecontact.addEventListener("click",removeContact)
+}
+
+function goToChatOrSendChatRequest(e){
+    console.log(e.target)
+    let todo = e.target.getAttribute("data-type");
+    if(todo === "sendrequest"){
+        //send chat request through sendtoserver
+        let receiver = parseInt(e.target.getAttribute("data-contactid"));
+        const data = {type: 'chatrequest', 'sendermid': parseInt(getMarsID()), 'receivercontactid': receiver, answer: 0};
+        console.log(data)
+        sendToServer(data)
+    }else{
+        //put the chat type in private(with chatid)
+        //store chatid in localstorage
+        //go to chatroom page
+        console.log(e.target.getAttribute("data-chatid"))
+    }
+}
+
+function removeContact(e){
+    e.preventDefault();
+    removeUserContact(e.target.getAttribute("data-contactid"));
+
+    setTimeout(function(){
+        updateContacts();
+    },1000)
+        
 }
 
 function searchInputField() {
@@ -61,4 +110,9 @@ function searchInputField() {
         }
     }
 
+}
+
+async function loadConfig() {
+    const response = await fetch("config.json");
+    return response.json();
 }
