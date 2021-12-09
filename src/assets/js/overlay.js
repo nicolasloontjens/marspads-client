79aab0b25b8d509de1450e19253247b112b539ea
stijn <stijn.follet@student.howest.be>
2021-12-09 16:11:01 +0100

    Added an overlay for the audiopage.

---
+++ src/assets/js/overlay.js
@@ -0,0 +1,17 @@
+
+"use strict";
+
+document.addEventListener("DOMContentLoaded", init);
+
+
+async function init() {
+    document.querySelector(".overlay").addEventListener("click",on);
+}
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
