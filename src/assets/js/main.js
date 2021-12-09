618a2ffc7f17ce9f22e3867a7fe167c4949f7f1b
nicolas.loontjens <nicolas.loontjens@student.howest.be>
2021-12-09 13:17:51 +0100

    added filter for people with existing chats

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,7 @@
 let config;
 let api;
+const VAPID_PUBLIC_KEY = "BDTYe4PbcJ4UuSl3kQCv7zjoy1eUXbN3ul3mhMk7KY5oBI7p0NBfLSFcn-Y-dU8SpqHUW60vAD8ehW_klnwyM7c"
+let sendToServer = null;
 
 document.addEventListener("DOMContentLoaded", init);
 
@@ -13,7 +15,45 @@ async function init() {
     addNavRedirects();
     document.querySelector('#arrowNav').addEventListener('click', MakeNavigationRetract);
 
+    sendToServer = openSocket();
+    Notification.requestPermission((status)=>{
+        registerPush();
+    })
+    if('serviceWorker' in navigator){
+        navigator.serviceWorker.register("./sw.js",{
+            Scope: '/'
+        }).catch(function(err){
+            console.log("error registering worker: ", err)
+        });
+        navigator.serviceWorker.addEventListener("message",(message) => {
+            let obj = messaga.data.msg
+            obj.receivercontactid = contactid
+            if(obj.answer != 0){
+                sendToServer(obj);
+            }
+        })
+    }
+}
 
+function registerPush(){
+    if('PushManager' in window){
+        navigator.serviceWorker.ready.then(registration => {
+            let subscribeOptions = {
+                userVisibleOnly: true,
+                applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY)
+            };
+            return registration.pushManager.subscribe(subscribeOptions);
+        }).then(pushSubscription => {
+            setTimeout(function(){
+                registerUser(pushSubscription);
+            },3000);//wait until user has definitely been generated and eventbus is ready
+        })
+    }
+}
+
+function registerUser(pushSubscription){
+    let mid = JSON.parse(localStorage.getItem("user")).marsid
+    sendToServer({"type":"subscription","marsid": mid, "subscription":pushSubscription});
 }
 
 async function loadConfig() {
@@ -48,3 +88,19 @@ function MakeNavigationRetract(e) {
     e.preventDefault();
     document.querySelector('main aside nav').classList.toggle('hidden');
 }
+
+//helper function for registering push notifications
+function urlBase64ToUint8Array (base64String){
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+      .replace(/\-/g, '+')
+      .replace(/_/g, '/');
+  
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+  
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+  };
