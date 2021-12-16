e256b6ab17c6887fe8f6cf5542b07019a961eb57
stijn <stijn.follet@student.howest.be>
2021-12-16 10:41:21 +0100

    Adding a rocket as loading page. 'Using animation'

---
+++ src/assets/js/loadingPage.js
@@ -0,0 +1,31 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", init);
+
+async function init() {
+    await addLoadingAnimation();
+    setTimeout(function () {
+        location.replace("index.html");
+    }, 9000);
+}
+
+
+async function addLoadingAnimation() {
+    document.querySelector("body").setAttribute("animation", "true")
+    document.querySelector("body").innerHTML += `<div class="animation">
+        <img class="rocket" src="assets/images/rocket.png">
+            <div class="longfazers">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <h1>loading</h1></div>`
+    await new Promise(resolve => setTimeout(resolve, 600))//display loading animation;
+}
+
+function removeAnimation() {
+    document.querySelector("body").removeAttribute("animation");
+    let elem = document.querySelector(".animation");
+    elem.remove();
+}
