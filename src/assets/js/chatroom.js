21d4dcd85f19de96e87fbda69a62bd7c575c6caa
stijn <stijn.follet@student.howest.be>
2021-12-20 16:52:30 +0100

    Removing code smells for chatroom.

--- src/assets/js/chatroom.js
+++ src/assets/js/chatroom.js
@@ -10,7 +10,7 @@ async function init(){
         //get chatmessages from server and display them
         await loadPrivateChatMessages();
         const chattername = await loadChatInfo();
-        document.querySelector("#username").innerHTML += `<p>${chattername}</p>`
+        document.querySelector("#username").innerHTML += `<p>${chattername}</p>`;
         document.querySelector("#send-button").addEventListener("click",sendPrivateMessage);
     }
 }
@@ -18,23 +18,23 @@ async function init(){
 function sendPublicMessage(e){
     e.preventDefault();
     const message = document.querySelector("#chat-message").value;
-    if(message != ""){
-        let user = JSON.parse(localStorage.getItem("user"))
+    if(message !== ""){
+        let user = JSON.parse(localStorage.getItem("user"));
         const data = {type: 'message',marsid:user.marsid,"message": message}
         sendToServer(data)
-        document.querySelector("#chat-message").value = "";
+        message.value = "";
     }
 }
 
 function sendPrivateMessage(e){
     e.preventDefault();
     const message = document.querySelector("#chat-message").value;
-    if(message != ""){
-        let user = JSON.parse(localStorage.getItem("user"));
-        let chatid = JSON.parse(localStorage.getItem("currentChatId"))
+    if(message !== ""){
+        const user = JSON.parse(localStorage.getItem("user"));
+        const chatid = JSON.parse(localStorage.getItem("currentChatId"));
         const data = {type:"privatemessage", "chatid":chatid, marsid: user.marsid, message: message}
         sendToServer(data);
-        document.querySelector("#chat-message").value = "";
+        message.value = "";
     }
 }
 
@@ -42,20 +42,20 @@ async function loadPrivateChatMessages(){
     const response = await getAllChatsWithUser(localStorage.getItem("currentChatId"))
     response.forEach(message => {
         let timestamp = message.timestamp;
-        timestamp = timestamp.substring(0,(timestamp.length-7))
-        document.querySelector("#messages").insertAdjacentHTML("beforeend",`<p class="chatMessage">${message.name} @ ${timestamp}: ${message.content}</p>`)
+        timestamp = timestamp.substring(0,(timestamp.length-7));
+        document.querySelector("#messages").insertAdjacentHTML("beforeend",`<p class="chatMessage">${message.name} @ ${timestamp}: ${message.content}</p>`);
     })
 }
 
 async function loadChatInfo(){
     const response = await getAllChats();
-    let currentChatId = localStorage.getItem("currentChatId")
-    currentChatId = parseInt(currentChatId)
+    let currentChatId = localStorage.getItem("currentChatId");
+    currentChatId = parseInt(currentChatId);
     let result;
     response.forEach(chat => {
         if(chat.chatid === currentChatId){
             result = chat;
         }
-    })
+    });
     return result.username;
 }
