aedcc91d8f8322a763bffd60740cbf8aed270d70
stijn <stijn.follet@student.howest.be>
2021-12-21 14:07:26 +0100

    Made the progressbar bug free.

--- src/assets/js/settingsPage.js
+++ src/assets/js/settingsPage.js
@@ -10,7 +10,8 @@ async function init() {
 
 
 function loadBar() {
-
+    let progress = document.querySelector("#progress");
+    document.querySelector("#startUpdate").removeEventListener("click", loadBar);
     let elem = document.querySelector("#progressBar");
     let width = 0;
     let interval = setInterval(frame, 50);
@@ -18,11 +19,13 @@ function loadBar() {
     function frame() {
         if (width >= 100) {
             clearInterval(interval);
-            document.querySelector("#progress").innerHTML = "Successfully updated";
+            progress.innerHTML = "Successfully updated";
+            document.querySelector("#startUpdate").addEventListener("click", loadBar);
         } else {
             width++;
             elem.style.width = width + '%';
-            document.querySelector("#number").innerHTML = width.toString();
+            progress.innerHTML = "Updating progress: " + width.toString() + "%";
+
         }
     }
 }
